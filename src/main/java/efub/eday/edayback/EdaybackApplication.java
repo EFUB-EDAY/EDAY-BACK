250deee4b7ee5e8f3e39231691e3e9ad64f5deed
Min A <min2010011@gmail.com>
2023-07-09 19:54:39 +0900

    Init spring project

---
+++ src/main/java/efub/eday/edayback/EdaybackApplication.java
@@ -0,0 +1,13 @@
+package efub.eday.edayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaybackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efub/eday/edayback/EdaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.eday.edayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
