81428f8f3744e06f961e2fb0df533249395d0b26
chhaewxn <chaewon1019@ewhain.net>
2023-07-18 20:24:28 +0900

    feat: SubjectRepository 파일 추가 #3

---
+++ src/main/java/efub/eday/edayback/domain/day/dday/repository/SubjectRepository.java
@@ -0,0 +1,8 @@
+package efub.eday.edayback.domain.day.dday.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import efub.eday.edayback.domain.day.dday.entity.Subject;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
