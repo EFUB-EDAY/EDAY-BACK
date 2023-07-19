9bf672a6f42ac3b12f4b6b155b91bdedaf6cdd38
choiyounji <yunji71@naver.com>
2023-07-19 22:05:10 +0900

    feat: JwtExceptionFilter에서 불필요한 부분 삭제

--- src/main/java/efub/eday/edayback/domain/config/JwtExceptionFilter.java
+++ src/main/java/efub/eday/edayback/domain/config/JwtExceptionFilter.java
@@ -1,16 +1,11 @@
 package efub.eday.edayback.domain.config;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import efub.eday.edayback.domain.global.exception.CustomErrorCode;
 import efub.eday.edayback.domain.global.exception.CustomException;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
@@ -34,44 +29,10 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
 		} catch (JwtException e) {
 			log.error("[-] Invalid Token");
 
-			// 오류 내용 기입
-			BaseResponse baseResponse = new BaseResponse(BaseResponseStatus.INVALID_JWT);
-
-			Map<String, Object> errorDetails = setErrorDetails(baseResponse);
-
-			sendErrorMessage(response, errorDetails);
 		} catch (CustomException e) {
 			log.error(e.getMessage());
 
-			// 헤더에 토큰이 비어있거나 잘못된 정보가 기입되었을 경우
-			if (e.getErrorCode().equals(CustomErrorCode.INVALID_AUTH)) {
-				BaseResponse baseResponse = new BaseResponse(BaseResponseStatus.EMPTY_JWT);
-
-				Map<String, Object> errorDetails = setErrorDetails(baseResponse);
-
-				sendErrorMessage(response, errorDetails);
-			}
 		}
 	}
 
-	// Set Error Json
-	private Map<String, Object> setErrorDetails(BaseResponse baseResponse) {
-		Map<String, Object> errorDetails = new HashMap<>();
-
-		errorDetails.put("isSuccess", baseResponse.getIsSuccess());
-		errorDetails.put("code", baseResponse.getCode());
-		errorDetails.put("message", baseResponse.getMessage());
-
-		return errorDetails;
-	}
-
-	// Send Error Message to Client
-	private void sendErrorMessage(HttpServletResponse response, Map<String, Object> errorDetails) throws IOException {
-
-		response.setStatus(HttpStatus.UNAUTHORIZED.value());
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		response.setCharacterEncoding("UTF-8");
-
-		objectMapper.writeValue(response.getWriter(), errorDetails);
-	}
 }
