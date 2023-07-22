1ddf8b10112be14d5e51f40df1a3b079fa6f6a55
Kwon Min A <min2010011@gmail.com>
2023-07-23 03:20:51 +0900

    feat(KakaoAuthFeign): 카카오 토큰 발급을 위한 Feign Client 추가

---
+++ src/main/java/efub/eday/edayback/domain/member/auth/service/feign/KakaoAuthFeign.java
@@ -0,0 +1,14 @@
+package efub.eday.edayback.domain.member.auth.service.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import efub.eday.edayback.domain.member.auth.dto.KakaoTokenRequestDto;
+import efub.eday.edayback.domain.member.auth.dto.KakaoTokenResponseDto;
+
+@FeignClient(name = "KakaoAuthFeign", url = "https://kauth.kakao.com/oauth/token")
+public interface KakaoAuthFeign {
+
+	@PostMapping(consumes = "application/x-www-form-urlencoded")
+	KakaoTokenResponseDto getKakaoToken(KakaoTokenRequestDto requestDto);
+}
