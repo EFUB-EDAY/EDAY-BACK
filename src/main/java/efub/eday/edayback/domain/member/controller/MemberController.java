0c3f3e7dee9794172827be317c830dd49a3759c0
Kwon Min A <min2010011@gmail.com>
2023-07-23 03:42:01 +0900

    chore: 불필요한 메소드 및 클래스 제거

--- src/main/java/efub/eday/edayback/domain/member/controller/MemberController.java
+++ src/main/java/efub/eday/edayback/domain/member/controller/MemberController.java
@@ -1,27 +1,15 @@
 package efub.eday.edayback.domain.member.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import efub.eday.edayback.domain.member.dto.LoginResponseDto;
-import efub.eday.edayback.domain.member.entity.Member;
-import efub.eday.edayback.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/members")
+@RequestMapping("/member")
 public class MemberController {
-	private final MemberService memberService;
-
-	@GetMapping("/oauth")
-	public LoginResponseDto kakaoLogin(@RequestParam String code) {
-		System.out.println("HI");
-		Member member = memberService.getAccessToken(code);
-		System.out.println(member);
-		return memberService.login(member);
-	}
 
 }
