67d69a22f981eb88f07daaa1bb5e6d565a89ca2e
choiyounji <yunji71@naver.com>
2023-07-29 04:27:02 +0900

    feat: auth 부분 swagger 추가

--- src/main/java/efub/eday/edayback/domain/member/auth/dto/KakaoTokenResponseDto.java
+++ src/main/java/efub/eday/edayback/domain/member/auth/dto/KakaoTokenResponseDto.java
@@ -1,5 +1,6 @@
 package efub.eday.edayback.domain.member.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,11 +9,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class KakaoTokenResponseDto {
-
+	@Schema(description = "토큰 타입")
 	private String token_type;
+	@Schema(description = "accessToken")
 	private String access_token;
+	@Schema(description = "accessToken 만료 기한")
 	private int expires_in;
+	@Schema(description = "refreshToken")
 	private String refresh_token;
+	@Schema(description = "refreshToken 만료 기한")
 	private int refresh_token_expires_in;
+	@Schema(description = "인가 코드")
 	private String scope;
 }
