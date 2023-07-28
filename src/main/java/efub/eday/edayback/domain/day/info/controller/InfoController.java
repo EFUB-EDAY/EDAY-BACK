b290b5c72398f8d2afa32baf30c8c69f4c2fb95c
choiyounji <yunji71@naver.com>
2023-07-29 04:04:21 +0900

    feat : info 부분 swagger 작성

--- src/main/java/efub/eday/edayback/domain/day/info/controller/InfoController.java
+++ src/main/java/efub/eday/edayback/domain/day/info/controller/InfoController.java
@@ -11,14 +11,30 @@ import efub.eday.edayback.domain.day.dday.entity.Dday;
 import efub.eday.edayback.domain.day.info.dto.InfoDto;
 import efub.eday.edayback.domain.day.info.entity.Info;
 import efub.eday.edayback.domain.day.info.service.InfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "추가정보", description = "추가정보 관련 api입니다.")
 @RestController
 @RequestMapping("/infos")
 @RequiredArgsConstructor
 public class InfoController {
 	private final InfoService infoService;
 
+	@Operation(summary = "추가정보 확인 메소드", description = "디데이 일수와 주제, 그리고 사진정보를 제공하는 메소드입니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "추가정보 확인 성공",
+			content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = InfoDto.class))),
+		@ApiResponse(responseCode = "404", description = "추가정보 확인 실패(존재하지 않는 디데이)",
+			content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = InfoDto.class)))
+	})
 	@GetMapping("/{d_day}")
 	@ResponseStatus(value = HttpStatus.OK)
 	public InfoDto getInfoByDday(@PathVariable("d_day") int dday) {
--- src/main/java/efub/eday/edayback/domain/day/info/dto/InfoDto.java
+++ src/main/java/efub/eday/edayback/domain/day/info/dto/InfoDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import efub.eday.edayback.domain.day.info.entity.Info;
 import efub.eday.edayback.domain.day.info.entity.InfoImage;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,11 @@ import lombok.Setter;
 public class InfoDto {
 	// private Long infoId;
 	// private Long infoImageId;
+	@Schema(description = "디데이 일수")
 	private int dday;
+	@Schema(description = "추가정보 주제")
 	private String headline;
+	@Schema(description = "사진 url 리스트")
 	private List<ImageDto> imageList;
 
 	public static InfoDto from(Info info) {
