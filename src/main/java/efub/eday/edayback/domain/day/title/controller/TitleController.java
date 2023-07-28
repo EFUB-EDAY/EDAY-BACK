b2e602ebe2f3dc73e29688404ae0e62f3563b92e
hannah0226 <mohan0226@naver.com>
2023-07-28 22:09:22 +0900

    feat: Title API 명세서 작성 (Swagger)

--- src/main/java/efub/eday/edayback/domain/day/title/controller/TitleController.java
+++ src/main/java/efub/eday/edayback/domain/day/title/controller/TitleController.java
@@ -1,5 +1,10 @@
 package efub.eday.edayback.domain.day.title.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +17,15 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/title")
 @RequiredArgsConstructor
+@Tag(name = "칭호 페이지", description = "칭호 페이지 관련 api입니다.")
 public class TitleController {
 
 	private final TitleService titleService;
 
+	@Operation(summary = "칭호 페이지 조회", description = "칭호 페이지를 조회하는 메서드입니다.")
+	@ApiResponse(responseCode = "200", description = "칭호 페이지 조회 성공",
+			   content = @Content(mediaType = "application/json",
+			   schema = @Schema(implementation = TitleResponseDto.class)))
 	@GetMapping()
 	public ResponseEntity<TitleResponseDto> getTitlePage() {
 		TitleResponseDto titleResponseDto = titleService.getTitlePage();
