64866ecde944d206ec796c1cf36d8aacfed2b6bb
hannah0226 <mohan0226@naver.com>
2023-07-28 21:54:17 +0900

    feat: Query API 명세서 작성 (Swagger)

--- src/main/java/efub/eday/edayback/domain/query/dto/QueryRequestDto.java
+++ src/main/java/efub/eday/edayback/domain/query/dto/QueryRequestDto.java
@@ -1,5 +1,6 @@
 package efub.eday.edayback.domain.query.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QueryRequestDto {
+	@Schema(description = "문의사항 내용", example = "D-3 내용 문의 드립니다!")
 	private String queryContent;
+	@Schema(description = "디데이", example = "3")
 	private int dday;
 }
