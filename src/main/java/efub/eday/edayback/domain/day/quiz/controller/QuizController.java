71d51398fa7555ae859db353a9cd42bc6bc2e1da
hannah0226 <mohan0226@naver.com>
2023-07-28 21:15:39 +0900

    feat: Quiz API 명세서 작성 (Swagger)

--- src/main/java/efub/eday/edayback/domain/day/quiz/controller/QuizController.java
+++ src/main/java/efub/eday/edayback/domain/day/quiz/controller/QuizController.java
@@ -1,23 +1,30 @@
 package efub.eday.edayback.domain.day.quiz.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+		import io.swagger.v3.oas.annotations.Operation;
+		import io.swagger.v3.oas.annotations.media.Content;
+		import io.swagger.v3.oas.annotations.media.Schema;
+		import io.swagger.v3.oas.annotations.responses.ApiResponse;
+		import io.swagger.v3.oas.annotations.responses.ApiResponses;
+		import io.swagger.v3.oas.annotations.tags.Tag;
+		import org.springframework.http.HttpStatus;
+		import org.springframework.http.ResponseEntity;
+		import org.springframework.web.bind.annotation.GetMapping;
+		import org.springframework.web.bind.annotation.PathVariable;
+		import org.springframework.web.bind.annotation.PostMapping;
+		import org.springframework.web.bind.annotation.RequestBody;
+		import org.springframework.web.bind.annotation.RequestMapping;
+		import org.springframework.web.bind.annotation.ResponseStatus;
+		import org.springframework.web.bind.annotation.RestController;
 
-import efub.eday.edayback.domain.day.quiz.dto.QuizAnswerResponseDto;
-import efub.eday.edayback.domain.day.quiz.dto.QuizRequestDto;
-import efub.eday.edayback.domain.day.quiz.dto.QuizResponseDto;
-import efub.eday.edayback.domain.day.quiz.entity.Quiz;
-import efub.eday.edayback.domain.day.quiz.service.QuizService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+		import efub.eday.edayback.domain.day.quiz.dto.QuizAnswerResponseDto;
+		import efub.eday.edayback.domain.day.quiz.dto.QuizRequestDto;
+		import efub.eday.edayback.domain.day.quiz.dto.QuizResponseDto;
+		import efub.eday.edayback.domain.day.quiz.entity.Quiz;
+		import efub.eday.edayback.domain.day.quiz.service.QuizService;
+		import lombok.RequiredArgsConstructor;
+		import lombok.extern.slf4j.Slf4j;
 
+@Tag(name="퀴즈", description = "퀴즈 관련 api입니다.")
 @Slf4j
 @RestController
 @RequestMapping("/quiz/{dDay}")
@@ -26,6 +33,15 @@ public class QuizController {
 	private final QuizService quizService;
 
 	//퀴즈 내용 조회
+	@Operation(summary = "퀴즈 내용 조회 메서드", description = "해당 디데이의 퀴즈를 조회하는 메서드입니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "퀴즈 내용 조회 성공",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = QuizResponseDto.class))),
+			@ApiResponse(responseCode = "404", description = "퀴즈 내용 조회 실패",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = QuizResponseDto.class)))
+	})
 	@GetMapping
 	@ResponseStatus(value = HttpStatus.OK)
 	public QuizResponseDto findQuiz(@PathVariable int dDay) {
@@ -34,10 +50,19 @@ public class QuizController {
 	}
 
 	//퀴즈 정답 확인
+	@Operation(summary = "퀴즈 정답 확인 메서드", description = "퀴즈 선지 중 하나를 보내면 정답 여부를 판별해 주는 메서드입니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "퀴즈 정답 확인 성공",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = QuizAnswerResponseDto.class))),
+			@ApiResponse(responseCode = "404", description = "퀴즈 정답 확인 실패",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = QuizAnswerResponseDto.class)))
+	})
 	@PostMapping
 	public ResponseEntity<QuizAnswerResponseDto> checkAnswer(
-		@PathVariable int dDay,
-		@RequestBody QuizRequestDto quizRequestDto
+			@PathVariable int dDay,
+			@RequestBody QuizRequestDto quizRequestDto
 	) {
 		boolean isCorrect;
 		String quizDescription = null;
