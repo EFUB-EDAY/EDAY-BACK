dfb0175ca7dc16261091ee9a9d3dd775dcf81c3e
Kwon Min A <min2010011@gmail.com>
2023-08-04 01:26:23 +0900

    feat: Title 엔티티에 텍스트 없는 이미지를 저장하는 컬럼 매핑 추가

--- src/main/java/efub/eday/edayback/domain/day/title/entity/Title.java
+++ src/main/java/efub/eday/edayback/domain/day/title/entity/Title.java
@@ -1,42 +1,32 @@
 package efub.eday.edayback.domain.day.title.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
 import efub.eday.edayback.domain.day.dday.entity.Subject;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Title {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "title_id")
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "title_id")
+    private Integer id;
 
-	@Column(name = "title_name", nullable = false, length = 127)
-	private String name;
+    @Column(name = "title_name", nullable = false, length = 127)
+    private String name;
 
-	@Column(name = "image_url", nullable = false)
-	private String imageUrl;
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
 
-	@OneToOne
-	@JoinColumn(name = "subject_id", nullable = false)
-	private Subject subject;
+    @Column(name = "image_with_not_text_url", nullable = false)
+    private String imageWithNotTextUrl;
 
-	@Builder
-	public Title(String name, String imageUrl, Subject subject) {
-		this.name = name;
-		this.imageUrl = imageUrl;
-		this.subject = subject;
-	}
+    @OneToOne
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
 }
