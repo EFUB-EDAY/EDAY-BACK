b009c82dae594d493acd0babfa12303a2e518c61
choiyounji <yunji71@naver.com>
2023-08-06 16:50:09 +0900

    feat: openList은 날짜에만 영향받도록 다시 수정

--- src/main/java/efub/eday/edayback/domain/member/service/MemberService.java
+++ src/main/java/efub/eday/edayback/domain/member/service/MemberService.java
@@ -1,5 +1,13 @@
 package efub.eday.edayback.domain.member.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import efub.eday.edayback.domain.day.title.entity.Title;
 import efub.eday.edayback.domain.day.title.repository.TitleRepository;
 import efub.eday.edayback.domain.member.auth.service.AuthService;
@@ -12,84 +20,70 @@ import efub.eday.edayback.domain.member.entity.MemberTitle;
 import efub.eday.edayback.domain.member.repository.MemberTitleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
-    private final AuthService authService;
-    private final TitleRepository titleRepository;
-    private final MemberTitleRepository memberTitleRepository;
+	private final AuthService authService;
+	private final TitleRepository titleRepository;
+	private final MemberTitleRepository memberTitleRepository;
 
-    @Transactional(readOnly = true)
-    public ProfileResDto getMember() {
-        //현재 로그인 중인 사용자 정보 불러오기
-        Member member = authService.getCurrentMember();
+	@Transactional(readOnly = true)
+	public ProfileResDto getMember() {
+		//현재 로그인 중인 사용자 정보 불러오기
+		Member member = authService.getCurrentMember();
 
-        int level = member.getLevel();
-        if (level > 7) {
-            level = 7;
-        }
+		int level = member.getLevel();
+		if (level > 7) {
+			level = 7;
+		}
 
-        //칭호는 dDay가 아닌 level을 기준으로 정해짐
-        Title title = titleRepository.findBySubjectId(level).orElseThrow();
+		//칭호는 dDay가 아닌 level을 기준으로 정해짐
+		Title title = titleRepository.findBySubjectId(level).orElseThrow();
 
-        //profile부분에 들어갈 속성들
-        ProfileDto profile = new ProfileDto(member.getNickname(),
-                member.getProfileImageUrl(),
-                member.getLevel(),
-                title.getName());
+		//profile부분에 들어갈 속성들
+		ProfileDto profile = new ProfileDto(member.getNickname(),
+			member.getProfileImageUrl(),
+			member.getLevel(),
+			title.getName());
 
-        // 현재 날짜와 데이터베이스에 저장된 날짜 사이의 차이 계산
-        LocalDateTime currentDate = LocalDateTime.now();
-        int differenceInDays = Math.toIntExact(ChronoUnit.DAYS.between(member.getCreatedDate(), currentDate));
-        if (differenceInDays >= 7) {
-            differenceInDays = 7;
-        }
+		// 현재 날짜와 데이터베이스에 저장된 날짜 사이의 차이 계산
+		LocalDateTime currentDate = LocalDateTime.now();
+		int differenceInDays = Math.toIntExact(ChronoUnit.DAYS.between(member.getCreatedDate(), currentDate));
+		if (differenceInDays >= 7) {
+			differenceInDays = 7;
+		}
 
-        List<QuizDto> doneList = new ArrayList<>();
-        List<MemberTitle> memberTitles = memberTitleRepository.findByMember(member);
-        for (MemberTitle memberTitle : memberTitles) {
-            if (memberTitle.getGetTitle()) {
-                int subject = memberTitle.getTitle().getSubject().getDday();
-                doneList.add(new QuizDto(subject));
-            }
-        }
+		//날짜가 같아 그러면 difference=0, openList는 dDay=7 하나
+		List<QuizDto> openList = new ArrayList<>();
+		for (int dDay = 7; dDay >= 7 - differenceInDays; dDay--) {
+			if (dDay != 0) {
+				openList.add(new QuizDto(dDay));
+			}
+		}
 
-        //날짜가 같아 그러면 difference=0, openList는 dDay=7 하나
-        List<QuizDto> openList = new ArrayList<>();
-        for (int dDay = 7; dDay >= 7 - differenceInDays; dDay--) {
-            boolean isInDoneList = false;
-            for (QuizDto quiz : doneList) {
-                if (quiz.getDday() == dDay) {
-                    isInDoneList = true;
-                    break;
-                }
-            }
-            if (!isInDoneList && dDay != 0) {
-                openList.add(new QuizDto(dDay));
-            }
-        }
+		List<QuizDto> doneList = new ArrayList<>();
+		List<MemberTitle> memberTitles = memberTitleRepository.findByMember(member);
+		for (MemberTitle memberTitle : memberTitles) {
+			if (memberTitle.getGetTitle()) {
+				int subject = memberTitle.getTitle().getSubject().getDday();
+				doneList.add(new QuizDto(subject));
+			}
+		}
 
-        List<QuizDto> closeList = new ArrayList<>();
-        for (int dDay = 6 - differenceInDays; dDay >= 1; dDay--) {
-            closeList.add(new QuizDto(dDay));
-        }
+		List<QuizDto> closeList = new ArrayList<>();
+		for (int dDay = 6 - differenceInDays; dDay >= 1; dDay--) {
+			closeList.add(new QuizDto(dDay));
+		}
 
-        QuizListDto quizListDto = new QuizListDto();
-        quizListDto.setOpenList(openList);
-        quizListDto.setDoneList(doneList);
-        quizListDto.setCloseList(closeList);
+		QuizListDto quizListDto = new QuizListDto();
+		quizListDto.setOpenList(openList);
+		quizListDto.setDoneList(doneList);
+		quizListDto.setCloseList(closeList);
 
-        return new ProfileResDto(profile, quizListDto);
-    }
+		return new ProfileResDto(profile, quizListDto);
+	}
 
 }
